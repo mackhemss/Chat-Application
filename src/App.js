19312838917eafafb88c552b3fdee18357d910b6
mackhemss <iamhemss.workmail@gmail.com>
2021-12-28 16:42:32 +0530

    chat changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeedComponent from './component/chatFeed';
+import LoginFormComponent from './component/LoginForm';
 import './App.css';
 
-function App() {
+const projectID = 'c3de56b4-955f-4d51-ba60-89caeeada8d8';
+
+const App = () => {
+    debugger
+  if (!localStorage.getItem('username')) return <LoginFormComponent />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeedComponent {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
